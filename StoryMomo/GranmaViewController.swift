8ecdae843373b8395fe383fa3d7c5f473196bdfe
Michiru <starffhwxx@gmil.com>
2019-08-02 11:47:37 +0800

    ２ページ目のアラートOK

--- StoryMomo/GranmaViewController.swift
+++ StoryMomo/GranmaViewController.swift
@@ -7,16 +7,39 @@
 //
 
 import UIKit
+import SCLAlertView
 
 class GranmaViewController: UIViewController {
+    
+    //パーツ
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textView2: UITextView!
+    
+    @IBOutlet weak var blank1: UITextField!
+    @IBOutlet weak var blank2: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
     
-
-  
-
 }
+    @IBAction func button(_ sender: UIButton) {
+        if blank1.text == "山" && blank2.text == "川"{
+            SCLAlertView().showNotice("Good answer!", subTitle: "You can go next")
+            
+        }else if blank1.text == "川" && blank2.text == "山"{
+            SCLAlertView().showInfo("It's almost correct...but... ", subTitle: "Focus on sentence")
+        }else{
+            SCLAlertView().showError("Something Wrong! ...or let's write in blank!", subTitle:"you can try anytimes!")
+            
+        }
+        
+        
+        
+ 
+    }
+   
+
 }
