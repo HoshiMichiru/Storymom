1378bde8a23bc062218a3b63d4aa96951340eef1
Michiru <starffhwxx@gmil.com>
2019-08-20 08:12:41 +0800

    no message

--- controllerView/Road1ViewController.swift
+++ controllerView/Road1ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import FontAwesome_swift
+
 
 class Road1ViewController: UIViewController {
 
+    @IBOutlet weak var LButton: UIButton!
+    @IBOutlet weak var Rbutton: UIButton!
+    @IBOutlet weak var Llabel: UILabel!
+    @IBOutlet weak var Rlabel: UILabel!
+    @IBOutlet weak var question: UITextView!
+    @IBOutlet weak var image: UIImageView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        LButton.titleLabel?.font = UIFont.fontAwesome(ofSize: 80
+            , style: .solid)
+       LButton.setTitle(String.fontAwesomeIcon(name: .arrowCircleLeft), for: .normal)
+        
+        Rbutton.titleLabel?.font = UIFont.fontAwesome(ofSize: 80, style: .solid)
+        Rbutton.setTitle(String.fontAwesomeIcon(name: .arrowCircleRight), for: .normal)
         
         
     }
